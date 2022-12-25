d557c9ff4db913efa0a9b3cfdcb8c81e1e050139
Bagiscan <buzmez01@gmail.com>
2022-12-25 12:55:14 +0300

    Ana oyuna Pause Menu Eklendi

---
+++ Assets/Scripts/PauseMenu/PauseMenuController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenuController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button PauseButton;
+    [SerializeField]
+    private GameObject PausePanel;
+
+    public void OnPauseButtonClicked()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0.01f;
+    }
+
+    public void OnContinueButtonClicked()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnMainMenuButtonClicked()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+
+    private IEnumerator LoadMainMenu()
+    {
+        yield return new WaitForSeconds(0.01f);
+        Time.timeScale = 1f;
+        if(MusicManager.Instance)
+        {
+            MusicManager.Instance.SetToMenuMusic();
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
