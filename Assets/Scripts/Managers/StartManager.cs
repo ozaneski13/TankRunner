fb1062771f9b256447eca3c92ca4eaa92845dfd0
Ozan Eşki <34617144+ozaneski13@users.noreply.github.com>
2023-01-07 14:03:03 +0300

    Minor fixes.

--- Assets/Scripts/Managers/StartManager.cs
+++ Assets/Scripts/Managers/StartManager.cs
@@ -11,8 +11,8 @@ public class StartManager : MonoBehaviour
 
         Player.Instance.Tank.TankHealth.IncreaseHealth(1000);
         Player.Instance.TankPrefabs[(int)(Player.Instance.CurrentTank)].gameObject.SetActive(true);
-        //Player.Instance.Tank.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation;
-        Player.Instance.Tank.transform.position = new Vector3(0, 0f, 15f);
+        Player.Instance.Tank.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None | RigidbodyConstraints.FreezeRotation;
+        Player.Instance.Tank.transform.position = new Vector3(0, 1f, 15f);
         Player.Instance.Tank.GetModel();
         Player.Instance.Tank.GetMaterials();
     }
